d46be64e1b98f5086f173098039e428e6b00eff5
Your Name <you@example.com>
2024-03-28 22:15:21 +0300

    AppOpener

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('s   g')
+import  AppOpener
+def start():
+    AppOpener.open("roll_up")
+    
 
+
+# -----------------------------------------------------------------------
+if __name__ == '__main__':
+    start()
