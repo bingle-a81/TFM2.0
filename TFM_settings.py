e960256e5f5af025f6fcafe4ffc5b74110e1585f
work@ya.com <work>
2024-03-30 10:00:08 +0300

    settings

--- TFM_settings.py
+++ TFM_settings.py
@@ -1,36 +1,84 @@
 from configparser import ConfigParser
 import os
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(base_path, "set.ini")
+class Get_config():
+    def __init__(self,name_file) -> None:
+        self._name=name_file
+        self.init()
+
+    def init(self):
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(base_path,self._name)
+        if os.path.exists(config_path):
+            self.cfg = ConfigParser()
+            self.cfg.optionxform=str
+            self.cfg.read(config_path, encoding='utf-8')
+        else:
+            print("Конфигурация не найдена!")
+            quit(-1)
+
+    def get_set(self,section,option):
+        return self.cfg.get(section, option)
+    
+    def get_set_default(self,option):
+        return self.cfg.get('default', option)    
+
+    def get_dict_section(self,section):
+        return {k:v.split(';') for k,v in self.cfg[section].items()}
+
+        # for key, val in cfg['NCExplorer'].items():
+#     d=key
+#     print (f'{d}:{val.split(';')}')
+
+
+
+
+config=Get_config('set.ini')
+# print(a.get_set_default('source'))
+# print(a.get_dict_section('NCExplorer'))
+    
+
+
 # проверка наличия файла настоек
-if os.path.exists(config_path):
-    cfg = ConfigParser()
-    cfg.read(config_path, encoding='utf-8')
-else:
-    print("Конфигурация не найдена!")
 
 
-SERVER = cfg.get("smtp", "server")
-PORT = cfg.get("smtp", "port")
-EMAIL = cfg.get("smtp", "email")
-PASSWD = cfg.get("smtp", "passwd")
+# a=cfg.get('default','MAIN_PATH')
+# b=cfg.get('default','PATH_FOR_CHECK')
+# path = os.path.realpath(a+b)
+# os.chdir(path)
+# os.mkdir('11')
+# print(a)
 
-TO_ADDR_MAIL = cfg.get("start", "TO_ADDR_MAIL")
-PATH_FOR_CHECK = cfg.get("start", "PATH_FOR_CHECK")  # папка проги со станков
-PATH_FOR_BASE = cfg.get("start", "PATH_FOR_BASE")  # папка УП/УП
-PATH_FOR_COPY_NEW_FILES = cfg.get("start", "PATH_FOR_COPY_NEW_FILES")  # копируем новые файлы
-ARCHIVE_PROGRAMM = cfg.get("start", "ARCHIVE_PROGRAMM")
-LOG_FILE = cfg.get("start", "LOG_FILE")
-LOG_FILE_DEBUG = cfg.get("start", "LOG_FILE_DEBUG")
-SOURCE = cfg.get("start", "source")
 
-CHAT_ID=cfg.get("telega", "chat_id")
-TOKEN=cfg.get("telega", "token")
-a=[]
-for (each_key, each_val) in cfg.items('mashins'):
-    d=tuple(x.capitalize() for x in each_key.split(' ') )
-    print (f'{d}:{each_val}')
+# SERVER = cfg.get("smtp", "server")
+# PORT = cfg.get("smtp", "port")
+# EMAIL = cfg.get("smtp", "email")
+# PASSWD = cfg.get("smtp", "passwd")
+
+# TO_ADDR_MAIL = cfg.get("start", "TO_ADDR_MAIL")
+# PATH_FOR_CHECK = cfg.get("start", "PATH_FOR_CHECK")  # папка проги со станков
+# PATH_FOR_BASE = cfg.get("start", "PATH_FOR_BASE")  # папка УП/УП
+# PATH_FOR_COPY_NEW_FILES = cfg.get("start", "PATH_FOR_COPY_NEW_FILES")  # копируем новые файлы
+# ARCHIVE_PROGRAMM = cfg.get("start", "ARCHIVE_PROGRAMM")
+# LOG_FILE = cfg.get("start", "LOG_FILE")
+# LOG_FILE_DEBUG = cfg.get("start", "LOG_FILE_DEBUG")
+# SOURCE = cfg.get("start", "source")
+
+# CHAT_ID=cfg.get("telega", "chat_id")
+# TOKEN=cfg.get("telega", "token")
+# a=[]
+
+
+# for key, val in cfg['NCExplorer'].items():
+#     d=key
+#     print (f'{d}:{val.split(';')}')
+    
+# for key in cfg['PttMain'].keys():
+#     print (key)
+
+# d={'FG':1,'HJ':2}
+# print(d.keys())
+
 
     # for (each_key, each_val) in cfg.items(each_section):
     #     print (f'{each_key.capitalize()}:{each_val}')
--- logging_settings.py
+++ logging_settings.py
@@ -1,26 +1,139 @@
-import logging
+import logging 
+# from logging import StreamHandler, Formatter, LogRecord
+import smtplib
 import telebot
-import TFM_settings as _settings
+from TFM_settings import config
+
+class TelegramBotHandler(logging.Handler):
+    def __init__(self, token: str, chat_id: str):
+        super().__init__()
+        self.token = token
+        self.chat_id = chat_id
+
+    def emit(self, record: logging.LogRecord):
+        message = self.format(record)
+        bot = telebot.TeleBot(self.token)
+        try:
+            bot.send_message(self.chat_id,message)
+        except:
+            pass
+
+
+class MegaHandler(logging.Handler):
+    def __init__(self, filename):
+        logging.Handler.__init__(self)
+        self.filename = filename
+
+    def emit(self, record: logging.LogRecord):
+        message = self.format(record)
+        with open(self.filename, 'a') as file:
+            file.write(message + '\n')
+
+class MegaEmail(logging.Handler):
+    def __init__(self,server,port,email,passwd):
+        logging.Handler.__init__(self)
+        self.server=server
+        self.port=port
+        self.email=email
+        self.passwd=passwd
+
+    def emit(self, record: logging.LogRecord) :
+        message=self.format(record)
+        charset = f'Content-Type: text/plain; charset=utf-8'
+        mime = 'MIME-Version: 1.0'
+        body = "\r\n".join((f"From: {self.email}", f"To: {self.email}",
+                            f"Subject: File log debug.log ", mime, charset, "", message))
+        # формируем тело письма
+        try:
+            # подключаемся к почтовому сервису
+            smtp = smtplib.SMTP(self.server, self.port)
+            smtp.starttls()
+            smtp.ehlo()
+            # логинимся на почтовом сервере
+            smtp.login(self.email,self.passwd)
+            # пробуем послать письмо
+            smtp.sendmail(self.email, self.email,body.encode('utf-8'))
+        except smtplib.SMTPException as err:
+            print('Что - то пошло не так...')
+            raise err
+        finally:
+            smtp.quit()
 
 
 
-# class TelegramBotHandler(logging.Handler):
-#     def __init__(self, token: str, chat_id: str):
-#         super().__init__()
-#         self.token = token
-#         self.chat_id = chat_id
+logger_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
 
-#     def emit(self, record):
-#         message = self.format(record)
-#         bot = telebot.TeleBot(self.token)
-#         try:
-#             bot.send_message(self.chat_id,message)
-#         except:
-#             pass
+    'formatters': {
+        'std_format': {
+            'format': '{asctime} - {levelname} - {name} - {message}',
+            'style': '{'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'std_format',
+            # 'filters': ['new_filter'],
+        },
+        'file': {
+            '()': MegaHandler,
+            'level': 'INFO',
+            'filename': set.LOG_FILE,
+            'formatter': 'std_format',
+        },
+        'file1': {
+            '()': MegaHandler,
+            'level': 'DEBUG',
+            'filename': set.LOG_FILE_DEBUG,
+            'formatter': 'std_format',
+        },
+        'email':{
+            '()':MegaEmail,
+            'level': 'ERROR',
+            'server':set.SERVER,
+            'port':set.PORT,
+            'email':set.EMAIL,
+            'passwd':set.PASSWD,
+            'formatter': 'std_format',
+        },
+        'telegram_handler': {
+            '()': TelegramBotHandler,
+            'level': 'WARNING',
+            'chat_id': set.CHAT_ID,
+            'token': set.TOKEN,
+            'formatter': 'std_format',
+        }
 
-bot = telebot.TeleBot(_settings.TOKEN)
-bot.send_message('-1001577560699','Hello')
+    },
+    'loggers': {
+        'app_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file','email','telegram_handler'],
+            # 'propagate': False
+        },
+        'json_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console','file'],
+        },
+        'telega_logger': {
+            'level': 'DEBUG',
+            'handlers': ['telegram_handler'],
+        },
+        'pyautogui_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file', 'file1'],
+        },
+        'to_database_logger': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file', 'file1'],
+        },
+        'email_logger':{
+            'level': 'DEBUG',
+            'handlers': ['email'],
+        }
 
-# b=logging.basicConfig('ff',)
-# a=Teleg('5060398934:AAE0O8b0Op5iNLKIeGNIpCz7Pr-q9tWdTPA','-1001577560699')
-# a.emit('hello')
+    },
+}
