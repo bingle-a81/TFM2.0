c819b2991ffc7780f2e8db5dfb0c5608b2945b5c
work@ya.com <work>
2024-04-04 11:24:48 +0300

    keyb

--- pyauto.py
+++ pyauto.py
@@ -1,21 +1,7 @@
 import pyautogui as py
 import os,time
+import keyboard
 
+
+keyboard.press_and_release('shift+s, space')
 # py.useImageNotFoundException()
-
-# time.sleep(5)
-# print(py.position())
-
-a=r'c:\Users\breat\.picture\Screenshot_1.png'
-# if os.path.isfile(a):
-#     print('jk')
-#     a = os.path.realpath(a)
-#     # os.startfile(a)
-a = os.path.realpath(a)
-print(a)
-b=py.locateOnScreen(a,region=(170,200,500,500), confidence=0.5)
-print(b)
-# ,region=(170,200,270,300), confidence=0.95
-
-# .locateOnScreen(x,region=(164,104,240,200), confidence=0.95)
-py.moveTo(470,400)
