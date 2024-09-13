101444e11917eab81918518a3d7c90ad2cca82ff
work@ya.com <work>
2024-09-13 13:29:51 +0300

    tsug-126v

--- TFM_settings.py
+++ TFM_settings.py
@@ -38,6 +38,8 @@ def picture(machine):
             return ("tsu08.png",)
         case "Tsugami BO126TF-III":
             return ("ts126.png",)
+        case "Tsugami BO126TF-V":
+            return ("ts1261.png",)
         case "SMEC-NS2100SY":
             return ("smec1.png", "smec2.png")
         case "Citizen Cincom L12(1)":
